9b952b8fd2561489d80384494abf443c5bc0f47c
Netibox <aszsave123@gmail.com>
2026-08-13 08:53:15 +0700

    Add files via upload

---
+++ th.js
@@ -0,0 +1,32 @@
+ {
+ "name": "หนังไทย3",
+ "author": "(=◍․̫◍=) ดูฟรีเพื่อความบันเทิงภายในที่พักอาศัย (=◍․̫◍=)",
+ "image": "https://285hdmovies.com/wp-content/uploads/2025/04/logo285.png",
+ "groups": [
+
+  {
+   "name": "ทวงคืน Fearless Love (2022)",
+   "image": "https://hd.movie285.com/wp-content/uploads/2023/06/Fearless-Love-2022.webp",
+   "stations": [
+    {
+     "name": "ทวงคืน Fearless Love (2022)",
+     "image": "https://hd.movie285.com/wp-content/uploads/2023/06/Fearless-Love-2022.webp",
+     "url": "https://ezycdn.stream/playlist-8pnhaa1q-master.m3u8",
+     "referer": "https://hd.movie285.com/"
+    }
+   ]
+  },
+  {
+   "name": "เฟื่อน More Than Friend (2022)",
+   "image": "https://hd.movie285.com/wp-content/uploads/2024/04/More-Than-Friend-2022.jpg",
+   "stations": [
+    {
+     "name": "เฟื่อน More Than Friend (2022)",
+     "image": "https://hd.movie285.com/wp-content/uploads/2024/04/More-Than-Friend-2022.jpg",
+     "url": "https://ezycdn.stream/playlist-hsletusd-master.m3u8",
+     "referer": "https://hd.movie285.com/"
+    }
+	  ]
+  }
+ ]
+}
